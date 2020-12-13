926c7033e23d3e2dcda6e5c9dc38ec9760daa10b
TarsChiu <2059192947@qq.com>
2020-12-13 15:41:24 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //dest: "./publish/dist",
+  dest: "./dist",
   head: [
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
